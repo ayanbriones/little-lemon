b8ae66a1713b3a06730cbb5fdb3fcb8427155b56
Bryanth Briones <ayanbriones@gmail.com>
2023-02-13 00:11:10 +0300

    Add Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,35 @@
 import React from 'react'
-
+import footerPhoto from '../assets/restauranfood.jpg'
 export default function Footer() {
-  return <footer className='footer'>Footer</footer>
+  return (
+    <footer className='footer'>
+      <img src={footerPhoto} height={300} alt='' />
+      <div className='doormat'>
+        <h4>
+          Doormat <br />
+          Navigation
+        </h4>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div className='contact'>
+        <h4>Contact</h4>
+        <p>Address</p>
+        <p>Phone number</p>
+        <p>Email@email.com</p>
+      </div>
+      <div className='social_media'>
+        <h4>Social Media Links</h4>
+        <p>Address</p>
+        <p>Phone number</p>
+        <p>Email@email.com</p>
+      </div>
+    </footer>
+  )
 }
