48eaa72c9beaf741b9ea891d8dfcc11996f360dd
Bryanth Briones <ayanbriones@gmail.com>
2023-02-14 01:02:19 +0300

    Design CallToAction section

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,14 +5,14 @@ export default function CallToAction() {
   return (
     <section className='call-to-action bg-green'>
       <div>
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
+        <h1 className='title fg-yellow'>Little Lemon</h1>
+        <h2 className='subtitle'>Chicago</h2>
+        <p className='lead-text'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
           voluptatum voluptate perferendis nobis, iste libero! Magni quod ipsum
           culpa esse!
         </p>
-        <button className='button'>Reserve a table</button>
+        <button className='button bg-yellow'>Reserve a table</button>
       </div>
       <div className='image-container'>
         <img src={heroImage} alt='Little Lemon Restaurant' />
