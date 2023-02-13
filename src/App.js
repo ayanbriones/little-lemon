3b5d4eadb5834aa32363604a0a357f29885703be
Bryanth Briones <ayanbriones@gmail.com>
2023-02-13 19:24:12 +0300

    Remove Header component and use Nav component as main header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
 import HomePage from './components/HomePage'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className='container'>
-      <Header />
+      <Nav />
       <HomePage />
       <Footer />
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import LittleLemonLogo from '../assets/Logo.svg'
 
 export default function Nav() {
   return (
-    <nav className='nav'>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Menu</li>
-        <li>Reservations</li>
-        <li>Order Online</li>
-        <li>Login</li>
-      </ul>
-    </nav>
+    <header className='main-header'>
+      <img src={LittleLemonLogo} alt='Little Lemon Logo' />
+      <nav className='nav'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
