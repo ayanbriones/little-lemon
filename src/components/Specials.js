984ee7e2cfa5e42135542beeab95eda3c0f3d7c8
Bryanth Briones <ayanbriones@gmail.com>
2023-02-14 23:52:05 +0300

    Fix Specials layout

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -4,11 +4,15 @@ import SpecialsCard from './SpecialsCard'
 export default function Specials() {
   return (
     <section className='specials'>
-      <h2>Specials</h2>
-      <button className='button'>Online Menu</button>
-      <SpecialsCard />
-      <SpecialsCard />
-      <SpecialsCard />
+      <header>
+        <h2>Specials</h2>
+        <button className='button bg-yellow'>Online Menu</button>
+      </header>
+      <div className='cards'>
+        <SpecialsCard />
+        <SpecialsCard />
+        <SpecialsCard />
+      </div>
     </section>
   )
 }
--- src/components/SpecialsCard.js
+++ src/components/SpecialsCard.js
@@ -3,16 +3,21 @@ import greekSalad from '../assets/greek salad.jpg'
 
 export default function SpecialsCard() {
   return (
-    <div className='card'>
-      <title>Greek Salat</title>
-      <p className='price'>$12.99</p>
-      <img src={greekSalad} height={150} alt='Card' />
-      <p className='description'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi
-        quo magnam amet, repellendus blanditiis labore culpa expedita soluta
-        veniam cumque sunt adipisci, commodi minus ut nemo repellat esse nihil?
-      </p>
-      <p className='delivery'>Order a delivery</p>
+    <div className='card bg-gray'>
+      <img src={greekSalad} alt='Card' />
+      <div className='details-container'>
+        <div className='card-title'>
+          Greek Salad
+          <span className='highlight-text fg-red'>$12.99</span>
+        </div>
+        <p className='description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi
+          quo magnam amet, repellendus blanditiis labore culpa expedita soluta
+          veniam cumque sunt adipisci, commodi minus ut nemo repellat esse
+          nihil?
+        </p>
+        <p className='delivery'>Order a delivery</p>
+      </div>
     </div>
   )
 }
