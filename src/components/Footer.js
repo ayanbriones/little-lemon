5d82735deea0f53e6a4ef7bd349464c4f9ecbe3a
Bryanth Briones <ayanbriones@gmail.com>
2023-02-16 00:47:16 +0300

    Fix Footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,32 @@ import footerPhoto from '../assets/restauranfood.jpg'
 export default function Footer() {
   return (
     <footer className='footer fg-green'>
-      <img src={footerPhoto} height={300} alt='' />
       <div className='doormat'>
-        <h4>
-          Doormat <br />
-          Navigation
-        </h4>
-        <ul className='nav-list'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Menu</li>
-          <li>Reservations</li>
-          <li>Order Online</li>
-          <li>Login</li>
-        </ul>
+        <div className='image-wrapper'>
+          <img src={footerPhoto} alt='' />
+        </div>
+        <div className='wrapper'>
+          <h4>
+            Doormat <br />
+            Navigation
+          </h4>
+          <ul className='nav-list'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </div>
       </div>
-      <div className='contact'>
+      <div className='contacts'>
         <h4>Contact</h4>
         <p>Address</p>
         <p>Phone number</p>
         <p>Email@email.com</p>
       </div>
-      <div className='social_media'>
+      <div className='social-media'>
         <h4>Social Media Links</h4>
         <p>Address</p>
         <p>Phone number</p>
