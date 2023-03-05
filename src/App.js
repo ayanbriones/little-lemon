366943b33d842cfaa4055d9f83cdc5083af17d9e
Bryanth Briones <ayanbriones@gmail.com>
2023-03-05 23:43:32 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import About from './components/About'
 import Footer from './components/Footer'
 import HomePage from './components/HomePage'
+import Reservations from './components/Reservations'
+import OrderOnline from './components/OrderOnline'
+import Menu from './components/Menu'
 import Nav from './components/Nav'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className='container'>
       <Nav />
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/reservations' element={<Reservations />} />
+        <Route path='/order-online' element={<OrderOnline />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
       <Footer />
     </div>
   )
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Main from './Main'
+import CallToAction from './CallToAction'
+import Chicago from './Chicago'
+import CustomersSay from './CustomersSay'
+import Specials from './Specials'
 
 export default function HomePage() {
   return (
-    <>
-      <Main />
-    </>
+    <main className='main'>
+      <CallToAction />
+      <Specials />
+      <CustomersSay />
+      <Chicago />
+    </main>
   )
 }
