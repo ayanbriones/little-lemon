a3560ae745578837fc76cc03d51412c41b59402d
Bryanth Briones <ayanbriones@gmail.com>
2023-03-17 03:49:40 +0300

    fix specials card

--- src/components/Specials/Specials.js
+++ src/components/Specials/Specials.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import SpecialsCard from '../SpecialsCard/SpecialsCard'
 import './Specials.css'
+import greekSaladImg from '../../assets/greek salad 269x209.jpg'
+import lemonDessertImg from '../../assets/lemon dessert.jpg'
+import gourmetImg from '../../assets/gourmet.jpeg'
 
 export default function Specials() {
   return (
@@ -11,9 +14,24 @@ export default function Specials() {
           <button className='button bg-yellow'>Online Menu</button>
         </header>
         <div className='cards'>
-          <SpecialsCard />
-          <SpecialsCard />
-          <SpecialsCard />
+          <SpecialsCard
+            img={greekSaladImg}
+            title='Greek Salad'
+            price={12.99}
+            info=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi quo magnam amet, repellendus blanditiis labore culpa expedita soluta veniam cumque sunt adipisci, commodi minus ut nemo repellat esse nihil?'
+          />
+          <SpecialsCard
+            img={lemonDessertImg}
+            title='Lemon Dessert'
+            price={11.99}
+            info=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi quo magnam amet, repellendus blanditiis labore culpa expedita soluta veniam cumque sunt adipisci, commodi minus ut nemo repellat esse nihil?'
+          />
+          <SpecialsCard
+            img={gourmetImg}
+            title='Gourmet Dessert'
+            price={10.99}
+            info=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi quo magnam amet, repellendus blanditiis labore culpa expedita soluta veniam cumque sunt adipisci, commodi minus ut nemo repellat esse nihil?'
+          />
         </div>
       </div>
     </section>
--- src/components/SpecialsCard/SpecialsCard.js
+++ src/components/SpecialsCard/SpecialsCard.js
@@ -1,21 +1,15 @@
 import React from 'react'
-import greekSalad from '../../assets/greek salad.jpg'
 
-export default function SpecialsCard() {
+export default function SpecialsCard({ img, title, price, info }) {
   return (
     <div className='card bg-gray'>
-      <img src={greekSalad} alt='Card' />
+      <img src={img} alt='Card' />
       <div className='wrapper'>
         <div className='card-title'>
-          Greek Salad
-          <span className='highlight-text fg-red'>$12.99</span>
+          {title}
+          <span className='highlight-text fg-red'>{price}</span>
         </div>
-        <p className='description fg-green'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi
-          quo magnam amet, repellendus blanditiis labore culpa expedita soluta
-          veniam cumque sunt adipisci, commodi minus ut nemo repellat esse
-          nihil?
-        </p>
+        <p className='description fg-green'>{info}</p>
         <p className='delivery'>Order a delivery</p>
       </div>
     </div>
