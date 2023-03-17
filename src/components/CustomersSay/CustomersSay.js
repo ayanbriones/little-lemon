fbfa2924d9b1d335899c9d274e1fccdcfd21d4d8
Bryanth Briones <ayanbriones@gmail.com>
2023-03-17 04:02:53 +0300

    fix customerssay

--- src/components/CustomersSay/CustomersSay.js
+++ src/components/CustomersSay/CustomersSay.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import CustomersSayCard from '../CustomersSayCard/CustomersSayCard'
 import './CustomersSay.css'
+import img1 from '../../assets/74.jpg'
+import img2 from '../../assets/90.jpg'
+import img3 from '../../assets/92.jpg'
+import img4 from '../../assets/94.jpg'
 
 export default function CustomersSay() {
   return (
@@ -9,10 +13,38 @@ export default function CustomersSay() {
         <h2>Testimonials</h2>
       </header>
       <div className='cards'>
-        <CustomersSayCard />
-        <CustomersSayCard />
-        <CustomersSayCard />
-        <CustomersSayCard />
+        <CustomersSayCard
+          img={img1}
+          name='Michael Ruiz'
+          review='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus
+        explicabo sed molestiae voluptatum aperiam dignissimos reprehenderit
+        tempora ipsam et perspiciatis itaque, aut rem! Fuga perspiciatis
+        distinctio molestiae cumque doloribus.'
+        />
+        <CustomersSayCard
+          img={img2}
+          name='Joel McDonalid'
+          review='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus
+        explicabo sed molestiae voluptatum aperiam dignissimos reprehenderit
+        tempora ipsam et perspiciatis itaque, aut rem! Fuga perspiciatis
+        distinctio molestiae cumque doloribus.'
+        />
+        <CustomersSayCard
+          img={img3}
+          name='Herbert Porter'
+          review='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus
+        explicabo sed molestiae voluptatum aperiam dignissimos reprehenderit
+        tempora ipsam et perspiciatis itaque, aut rem! Fuga perspiciatis
+        distinctio molestiae cumque doloribus.'
+        />
+        <CustomersSayCard
+          img={img4}
+          name='Gina Hunter'
+          review='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus
+        explicabo sed molestiae voluptatum aperiam dignissimos reprehenderit
+        tempora ipsam et perspiciatis itaque, aut rem! Fuga perspiciatis
+        distinctio molestiae cumque doloribus.'
+        />
       </div>
     </section>
   )
--- src/components/CustomersSayCard/CustomersSayCard.js
+++ src/components/CustomersSayCard/CustomersSayCard.js
@@ -1,21 +1,15 @@
 import React from 'react'
-import cardPhoto from '../../assets/74.jpg'
-export default function CustomersSayCard() {
+export default function CustomersSayCard({ img, name, review }) {
   return (
     <div className='card bg-gray'>
-      <p>Rating</p>
+      <p>Rating ⭐️⭐️⭐️⭐️⭐️</p>
       <div className='wrapper'>
         <div className='image-wrapper'>
-          <img src={cardPhoto} alt='' />
+          <img src={img} alt='' />
         </div>
-        <p>Michael Ruiz</p>
+        <p>{name}</p>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus
-        explicabo sed molestiae voluptatum aperiam dignissimos reprehenderit
-        tempora ipsam et perspiciatis itaque, aut rem! Fuga perspiciatis
-        distinctio molestiae cumque doloribus.
-      </p>
+      <p>{review}</p>
     </div>
   )
 }
