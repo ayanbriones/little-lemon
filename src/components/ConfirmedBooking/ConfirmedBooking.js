92b9f07c9cc9271e93958fdbdc44ae4f7ce3e6b4
Bryanth Briones <ayanbriones@gmail.com>
2023-03-18 10:46:29 +0300

    cleanup confirmed booking

--- src/components/ConfirmedBooking/ConfirmedBooking.js
+++ src/components/ConfirmedBooking/ConfirmedBooking.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Check from '../Check/Check'
 import './ConfirmedBooking.css'
 
-export default function ConfirmedBooking(props) {
+export default function ConfirmedBooking() {
   return (
     <section className='confirmed-booking'>
       <Check />
       <h2>Reservation Confirmed!</h2>
-      <p>Date {props.date}</p>
-      <p>Time {props.time}</p>
     </section>
   )
 }
